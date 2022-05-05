4f41d11a4f1ed186babcddc719df03fe169936f2
Petci <101003958+Petci@users.noreply.github.com>
2022-05-05 10:43:22 +0200

    Add files via upload

---
+++ JavaApplication3.java
@@ -0,0 +1,11 @@
+package javaapplication3;
+
+public class JavaApplication3 {
+
+    public static void main(String[] args) {
+        int[][] test = new int[10][9];
+        System.out.println(test.length);
+        System.out.println(test[0].length);
+    }
+    
+}
